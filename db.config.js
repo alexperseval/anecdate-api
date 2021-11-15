4df7ba4a33b3a31248b84a63a875e2df360a8dfb
Alexandre Perseval <alexandre.perseval@icloud.com>
2021-11-15 15:58:27 +0100

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "anecdate-db"
+    host: "remotemysql.com",
+    user: "Ssg6Qm9A0E",
+    password: "nfg216gOJL",
+    database: "Ssg6Qm9A0E"
 };
